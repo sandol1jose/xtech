f4746b74ffa0f52c160b0bbb8b00c38ccba3e872
Jose <sandol1jose@gmail.com>
2024-09-13 12:34:58 -0600

    Primera version del catalogo

---
+++ assets/pdf/index.php
@@ -0,0 +1,5 @@
+<?php
+// Redirige a index.php
+header("Location: ../../index.html");
+exit(); // Asegura que el script se detenga después de la redirección
+?>
---
+++ qr.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Generador de QR</title>
+  <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
+</head>
+<body>
+  <div id="qrcode"></div>
+  <button id="download">Descargar como PNG</button>
+  <script>
+    // Crear el código QR
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+      text: "https://x-techgt.com/",
+      width: 450,
+      height: 450
+    });
+
+    // Función para descargar el código QR como PNG
+    document.getElementById("download").addEventListener("click", function() {
+      var canvas = document.querySelector("#qrcode canvas");
+      var img = canvas.toDataURL("image/png");
+
+      var link = document.createElement('a');
+      link.href = img;
+      link.download = 'qrcode.png';
+      link.click();
+    });
+  </script>
+</body>
+</html>
